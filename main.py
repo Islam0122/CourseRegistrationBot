57785ba0b0fd6402b37e6622ff9e4201d32b3231
kelsinaiM <mirbekobak@mail.ru>
2026-03-09 11:39:39 -0200

    save

--- main.py
+++ main.py
@@ -12,6 +12,25 @@ Token = os.getenv("Token")
 bot = Bot(token=Token)
 dp = Dispatcher()
 
+@dp.message(Command('start'))
+async def start(messege: Message):
+    await messege.answer_photo(
+        photo=FSInputFile("photo/img_academy.jpg"),
+        caption=f"""Здесь вы можете узнать о курсах и зарегистрироваться."Напишите
+/help чтобы увидеть все команды."""
+    )
+
+@dp.message(Command('help'))
+async def help(messege: Message):
+    await messege.answer_photo(
+        photo=FSInputFile("photo/img_academy.jpg"),
+        caption="📚 Доступные команды:\n\n"
+        "/start — начать работу с ботом\n"
+        "/aboutacademy — информация об академии\n"
+        "/aboutbot — информация о боте\n"
+        "/help — список команд"
+    )
+
 @dp.message(Command('aboutacademy'))
 async def aboutacademy(messege: Message):
     await messege.answer_photo(
